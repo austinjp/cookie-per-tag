fdf00bdc9db6b741382ba806a09075b535328368
austinjp <austinjp@users.noreply.github.com>
2015-10-27 12:03:34 +0000

    Stub functions

--- cookie-tag.php
+++ cookie-tag.php
@@ -1,3 +1,20 @@
 <?php
+  /*
+    Plugin Name: Cookie Tag
+    Plugin URI: http://example.com
+    Description: Plugin for tracking tag views using a cookie
+    Author: Austin Plunkett
+    Version: 0.0.1
+    Author URI: http://example.com
+  */
+
+function cookietag_admin_actions() {
+  add_options_page("Cookie Tag", "Cookie Tag", 1, "Cookie Tag", "cookietag_admin");
+}
+add_action('admin_menu', 'cookietag_admin_actions');
+
+function cookietag_admin() {
+  
+}
 
 ?>
